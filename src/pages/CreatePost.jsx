25c6dc1a1cd22ad72344ad8a4bed22b17852105a
amiraunyasopon <amiraunyasopon@gmail.com>
2025-11-15 16:18:12 -0800

    landing and createpost

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,9 +1,66 @@
+import { useState } from 'react';
+import { supabase } from '../client';
 import "./CreatePost.css"
 
 const CreatePost = () => {
+    const [post, setPost] = useState({ title: "", description: "", image: "" })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPost((prev) => (
+            {
+                ...prev,
+                [name]: value
+            }
+        ))
+    }
+
+    const createPost = async (e) => {
+        e.preventDefault();
+
+        await supabase
+            .from("Posts")
+            .insert({ title: post.title, description: post.description, image: post.image })
+            .select();
+
+        window.location = "/"
+    }
+
     return (
-        <>
-        </>
+        <div className="create-post">
+            <form>
+                <h1>Title</h1>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={post.value}
+                    onChange={handleChange}
+                    className="form-input"
+                />
+                <h1>Description</h1>
+                <input
+                    type="text"
+                    id="description"
+                    name="description"
+                    value={post.description}
+                    onChange={handleChange}
+                    className="form-input"
+                />
+                <h1>Image</h1>
+                <input
+                    type="text"
+                    id="image"
+                    name="image"
+                    value={post.image}
+                    onChange={handleChange}
+                    className="form-input"
+                />
+                <button type="submit" onClick={createPost} className="submit-button">
+                    Post
+                </button>
+            </form>
+        </div>
     )
 }
 export default CreatePost
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../client'
+import Post from '../components/Post'
 import "./Landing.css"
 
 const Landing = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const { data } = await supabase
+                .from("Posts")
+                .select()
+                .order("created_at", { ascending: true })
+            setPosts(data)
+        }
+        fetchPosts()
+    }, [])
+
     return (
-        <>
-            <div>Landing Test</div>
-        </>
+        <div className="readPosts">
+            {/* maps all the posts in the 'Posts' table */}
+            {
+                posts && posts.length > 0 ?
+                    [...posts]
+                        .sort((a, b) => a.id - b.id)
+                        .map((post, index) =>
+                            <Post
+                                key={index}
+                                creationTime={post.created_at}
+                                title={post.title}
+                                upvotes={post.upvotes}
+                            />
+                        ) : <h2>{"No Posts Yet"}</h2>
+            }
+        </div>
     )
 }
 export default Landing
